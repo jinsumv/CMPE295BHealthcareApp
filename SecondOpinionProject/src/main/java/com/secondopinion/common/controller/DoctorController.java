58a98c86fb47d467a93937bbb3e340d9b52f8edd
suryand@gmail.com <suryand@gmail.com>
2013-11-18 01:56:39 -0800

    import statement that was missed.

--- SecondOpinionProject/src/main/java/com/secondopinion/common/controller/DoctorController.java
+++ SecondOpinionProject/src/main/java/com/secondopinion/common/controller/DoctorController.java
@@ -13,6 +13,7 @@ import net.tanesha.recaptcha.ReCaptchaResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
